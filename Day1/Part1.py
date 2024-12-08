5d5be038b4764526ea7ac8287f6424a7a87e4a8a
Mark van der Werf <werfmark@gmail.com>
2024-12-08 13:03:01 +0100

    WIP

--- Day1/Part1.py
+++ Day1/Part1.py
@@ -1,4 +1,15 @@
-#  Write a function that adds two integers
+import os
 
-print('Test Git Setup')
-print('Test debugging')
+script_dir = os.path.dirname(__file__)
+input_file_path = os.path.join(script_dir, 'inputData.txt')
+
+list1 = []
+list2 = []
+
+with open(input_file_path, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        list1.append(int(line.strip().split()[0]))
+        list2.append(int(line.strip().split()[1]))
+
+# work inprogress, srot lists and substract pairwise
