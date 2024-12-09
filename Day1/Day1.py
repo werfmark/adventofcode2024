1962268c692e9c70180af7a4ab7c1253b7c9d53c
Mark van der Werf <werfmark@gmail.com>
2024-12-09 23:10:03 +0100

    both questions

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -12,15 +12,24 @@ with open(input_file_path, 'r') as file:
         list1.append(int(line.strip().split()[0]))
         list2.append(int(line.strip().split()[1]))
 
-# work inprogress, srot lists and substract pairwise
 
+#part1
 list1.sort()
 list2.sort()
 
-total_difference = 0
-for a, b in zip(list1, list2):
-    total_difference += abs(a - b)
-
+total_difference = sum(abs(a - b) for a, b in zip(list1, list2))
 print(total_difference)
+
+#part2
+similarity_score = sum(a * list2.count(a) for a in list1)
+print(similarity_score)
+
+
+
+
+
+
+
+
 
 
