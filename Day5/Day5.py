d6c8afac05997627b5b3ff0856f051862a972899
Mark van der Werf <werfmark@gmail.com>
2024-12-10 17:09:35 +0100

    Day5 WIP

---
+++ Day5/Day5.py
@@ -0,0 +1,6 @@
+orderings = [line.strip() for line in open("Day5\OrderingInput.txt").readlines()]
+
+jagged_array = [[int(x) for x in line.strip().split(',')] for line in open("Day5\PrintingsInput.txt").readlines()]
+
+
+
